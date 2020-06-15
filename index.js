9d8650b458b181a8ce4d74de0a24971b354fd867
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-06-15 12:44:46 +0900

    #2.7 middleware package installed

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from "express"
+import logger from "morgan"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import helmet from "helmet"
 
 const app = express();
 const PORT = 4000;
 
+const handleHome = (req, res) => res.send("Hello, welcome to my home!");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(logger("dev"));
+
+app.get("/", handleHome)
+
 app.listen(PORT, () => {
     console.log(`✅ listening on http://localhost:${PORT}`)
-});
+}); 
