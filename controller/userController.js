64eac10235d0e5b3e61b3e6cc01a3047ef520fb4
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-06-17 07:41:39 +0900

    #2.11 MVC Pattern part3

---
+++ controller/userController.js
@@ -0,0 +1,3 @@
+export const join = (req, res) => res.send("Join");
+export const logout = (req, res) => res.send("Logout");
+export const login = (req, res) => res.send("Login");
---
+++ controller/videoController.js
@@ -0,0 +1,2 @@
+export const videoHome = (req, res) => res.send("Home");
+export const videoSearch = (req, res) => res.send("Search"); 
